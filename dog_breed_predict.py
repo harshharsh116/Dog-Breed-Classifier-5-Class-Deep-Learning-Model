325a1e72f4e21befefa9e5944be8b4bddd7fdc69
harshharsh116 <129247142+harshharsh116@users.noreply.github.com>
2026-05-28 13:37:15 +0530

    Update dog_breed_predict.py

--- dog_breed_predict.py
+++ dog_breed_predict.py
@@ -40,7 +40,7 @@ uploaded_file = st.file_uploader(
 if uploaded_file is not None:
     image = Image.open(uploaded_file).convert("RGB")
 
-    st.image(image, caption="Uploaded Image", use_container_width=True)
+    st.image(image, caption="Uploaded Image")
 
     # Resize image
     img = image.resize((224, 224))
