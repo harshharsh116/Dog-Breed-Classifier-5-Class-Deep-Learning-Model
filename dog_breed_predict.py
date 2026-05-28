becd161da4302186de8a802e32fce3a5046987f2
harshharsh116 <129247142+harshharsh116@users.noreply.github.com>
2026-05-28 13:32:49 +0530

    Update dog_breed_predict.py

--- dog_breed_predict.py
+++ dog_breed_predict.py
@@ -2,8 +2,26 @@ import streamlit as st
 import tensorflow as tf
 import numpy as np
 from PIL import Image
+import gdown
+import os
+model_path = "dog_breed_model.keras"
 
-model = tf.keras.models.load_model('dog_breed_model.keras')
+if not os.path.exists(model_path):
+
+    file_id = "1tOxHB1dVljXXO3sroJCjV-tyWDLHrT9u"
+
+    url = f"https://drive.google.com/uc?id={file_id}"
+
+    gdown.download(url, model_path, quiet=False)
+
+
+
+@st.cache_resource
+def load_model():
+    return tf.keras.models.load_model(model_path)
+
+model = load_model()
+
 class_names = ['french_bulldog', 'german_shepherd', 'golden_retriever', 'poodle', 'yorkshire_terrier']
 st.title("🐶 Dog Breed Classifier")
 st.info(
